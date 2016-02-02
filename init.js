9e26be0e1ee0ad24d0ac51b69e46a64fea8db197
keleko34 <gregory.guidero@gmail.com>
2016-02-02 09:04:50 +0100

    added init file, package, and settings

---
+++ init.js
@@ -0,0 +1,14 @@
+var CreateInit = (function(){
+  function CreateInit()
+  {
+    function Init()
+    {
+      
+    }
+    
+    return Init;
+  }
+  return CreateInit;
+}());//include requires here
+
+CreateInit()
