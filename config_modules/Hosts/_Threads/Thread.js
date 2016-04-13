218877df39d21ba4f35d1bb29f485e1ac06d999f
keleko34 <gguidero@dataart.com>
2016-04-13 10:23:44 +0200

    env and thread update

--- config_modules/Hosts/_Threads/Thread.js
+++ config_modules/Hosts/_Threads/Thread.js
@@ -5,20 +5,42 @@ module.exports = (function(){
       , _modules = {}
       , _moduleList = []
       , _process = {}
+      , _environment = 'master'
+      , _environmentEnum = ['master','thread']
+      , _onMasterCall = function(){}
+      , _onThreadCall = function(){}
 
     function Thread()
     {
-
+      if(_environment === 'master')
+      {
+        _onMasterCall(Thread);
+      }
+      else
+      {
+        _onThreadCall(Thread);
+      }
     }
 
     Thread.id = function(v)
     {
-
+      if(v === undefined)
+      {
+        return _id;
+      }
+      _id = (typeof v === 'string' ? v : _id);
+      return Thread;
     }
 
     Thread.modules = function(v)
     {
-
+      if(v === undefined)
+      {
+        return _modules;
+      }
+      _modules = (typeof v === 'object' ? v : _modules);
+      _moduleList = Object.keys(_modules);
+      return Thread;
     }
 
     Thread.moduleList = function()
@@ -26,26 +48,66 @@ module.exports = (function(){
       return _moduleList;
     }
 
-    Thread.addModule = function(v)
+    Thread.addModule = function(v,active)
     {
-
+      if(typeof v === 'string')
+      {
+        _modules[v] = !!active;
+        _moduleList = Object.keys(_modules);
+        Thread.call();
+      }
     }
 
     Thread.removeModule = function(v)
     {
-
+      if(typeof v === 'string')
+      {
+        _modules[v] = null;
+        delete _modules[v];
+        _moduleList = Object.keys(_modules);
+        Thread.call();
+      }
     }
 
     Thread.environment = function(v)
     {
-
+      if(v === undefined)
+      {
+        return _environment;
+      }
+      _environment = (_environmentEnum.indexOf(v) > -1 ? v : _environment);
+      return Thread;
     }
 
-    Thread.process = function()
+    Thread.process = function(v)
     {
-      return (process.send !== undefined ? process : _process);
+      if(typeof v === undefined)
+      {
+        return (process.send !== undefined ? process : _process);
+      }
+      _process = (typeof v === 'object' ? v : _process);
+      return Thread;
     }
 
+    Thread.onMasterCall = function(v)
+    {
+      if(v === undefined)
+      {
+        return _onMasterCall;
+      }
+      _onMasterCall = (typeof v === 'function' ? v : _onMasterCall);
+      return Thread;
+    }
+
+    Thread.onThreadCall = function(v)
+    {
+      if(v === undefined)
+      {
+        return _onThreadCall;
+      }
+      _onThreadCall = (typeof v === 'function' ? v : _onThreadCall);
+      return Thread;
+    }
 
     return Thread;
   }
