6790cbded7861e7e734b28a0cfcae604d84d9ed5
keleko34 <gguidero@dataart.com>
2016-04-13 18:23:33 +0200

    vhost config update

--- admin/config_modules/_Vhost/Vhost.js
+++ admin/config_modules/_Vhost/Vhost.js
@@ -1,7 +1,9 @@
 var jsonfile_module = require('jsonfile')
+  , rmdir_module = require('rmdir')
+  , ncp_module = require('ncp')
   , fs_module = require('fs')
 
-module.exports = (function(jsonfile,fs){
+module.exports = (function(jsonfile,fs,rmdir,ncp){
   function CreateVshost()
   {
     var _path = ''
@@ -29,17 +31,112 @@ module.exports = (function(jsonfile,fs){
       return _config;
     }
 
-    Vshost.addHost = function(h)
+    Vshost.update = function()
     {
-
+      jsonfile.writeFile(_path,_config,{space:1},function(err){
+        if(err) console.log('ERR:','failed to save vhost config',err);
+      })
     }
 
+    //create app directory and all environments, create settings folder as well
+    Vshost.addHost = function(h)
+    {
+      if(_config[h] === undefined)
+      {
+        var checks = {
+          app:false,
+          dev:false,
+          qa:false,
+          settings:false
+        }
+        , finished = function()
+        {
+          _config[h] = '/'+h;
+          Vshost.update();
+        }
+        , check = function()
+        {
+          if(checks.app && checks.dev && checks.qa && checks.settings)
+          {
+            finished();
+          }
+        }
+
+        //view directory
+        fs.mkdir(ksprocess.base()+'/view/'+h,function(err){
+          if(!err)
+          {
+            fs.mkdir(ksprocess.base()+'/view/'+h+'/app',function(err){
+              if(!err)
+              {
+                checks.app = true;
+                check();
+              }
+            });
+
+            fs.mkdir(ksprocess.base()+'/view/'+h+'/dev',function(err){
+              if(!err)
+              {
+                checks.dev = true;
+                check();
+              }
+            });
+
+            fs.mkdir(ksprocess.base()+'/view/'+h+'/qa',function(err){
+              if(!err)
+              {
+                checks.qa = true;
+                check();
+              }
+            });
+          }
+        });
+
+        //settings dir
+        fs.mkdir(ksprocess.base()+'/settings/hosts/'+h,function(err){
+          if(!err)
+          {
+            fs.readDir(ksprocess.base()+'/admin/templates/config/host',function(err,dir){
+              if(!err)
+              {
+                var finCount = 0,
+                    finCb = function()
+                {
+                  if(finCount === dir.length)
+                  {
+                    checks.settings = true;
+                    check();
+                  }
+                }
+                //we will copy each file to settings
+                for(var x=0;x<dir.length;x+=1)
+                {
+                  console.log(dir);
+
+                }
+              }
+            });
+          }
+        });
+      }
+    }
+
+    //this will rec remove all view files and settings files
     Vhost.removeHost = function(h)
     {
-
+      if(_config[h] !== undefined)
+      {
+        rmdir(ksprocess.base()+'/view/'+h,function(err){
+          if(!err)
+          {
+            delete _config[h];
+            Vshost.update();
+          }
+        })
+      }
     }
 
     return Vshost;
   }
   return CreateVshost;
-}(jsonfile_module,fs_module));
+}(jsonfile_module,fs_module,rmdir_module,ncp_module));
